397a2e5ced529acfb5439fa8dd438aee2fca1cde
user301 <romanyakushev91@gmail.com>
2019-10-24 16:24:06 +0300

    commit 5 first initialization room

--- app/src/main/java/com/example/myvoting/app/activities/MainActivity.java
+++ app/src/main/java/com/example/myvoting/app/activities/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.example.myvoting.R;
 
@@ -30,22 +29,24 @@ public class MainActivity extends MvpAppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_list_users, R.id.nav_voting, R.id.nav_add_new_user, R.id.nav_information)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
